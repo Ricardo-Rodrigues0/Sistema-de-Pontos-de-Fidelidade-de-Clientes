e2f289644c267553a7a02071641ecf41fb34dcc1
Ricardo <ricardo272530@gmail.com>
2024-10-14 11:39:15 -0300

    .

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,11 @@ import './Style/styleHome.css';
 
 function Home() {
     return (
-        <div className="container">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary w-100">
+        <div className='mainHome'>
+            <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <a class="navbar-brand" href="/">
-                        <img src="/img/localiza-logo.png" alt="" width="220" height="50" class="d-inline-block align-text-center" />
+                    <a className="navbar-brand" href="/">
+                        <img src="/img/localiza-logo.png" alt="Localiza Logo" />
                     </a>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -17,10 +17,23 @@ function Home() {
                                 <a className="nav-link active" aria-current="page" href="/">
                                     Início
                                 </a>
-
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">
+                                    Regastar código
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">
+                                    Histórico
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">
+                                    Grupos
+                                </a>
                             </li>
                         </ul>
-
                         <div>
                             <Link to="/login" className="btn btn-primary mr-2">
                                 Login
@@ -33,6 +46,7 @@ function Home() {
                 </div>
             </nav>
 
+
             <section className='section1'>
                 <h1 className="mt-5 text-center">Conheça nossos carros da categoria Prime</h1>
                 <p className="lead text-center mb-5">Escolha o seu modelo favorito e acumule pontos!</p>
@@ -40,7 +54,7 @@ function Home() {
                 <div id="carouselExampleRide" className="carousel slide" data-bs-ride="true">
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <img src="/img/groupCar/prime/AUD3.png" className="d-block mx-auto w-50" alt="Haval H6" />
+                            <img src="/img/groupCar/prime/HAVA.png" className="d-block mx-auto w-10" alt="Haval H6" />
 
                             <div className="text-center p-3 mb-5 d-none d-md-block bg-dark bg-opacity-50 p-3 rounded mt-5 mt-5">
                                 <h4 className="text-white">Haval H6</h4>
@@ -49,7 +63,7 @@ function Home() {
                         </div>
 
                         <div className="carousel-item">
-                            <img src="/img/groupCar/prime/CMDR.png" className="d-block mx-auto w-50" alt="Jeep Commander" />
+                            <img src="/img/groupCar/prime/CMDR.png" className="d-block mx-auto w-10" alt="Jeep Commander" />
 
                             <div className="text-center p-3 mb-5 d-none d-md-block bg-dark bg-opacity-50 p-3 rounded mt-5 mt-5">
                                 <h4 className="text-white">Jeep Commander</h4>
@@ -59,7 +73,7 @@ function Home() {
 
 
                         <div className="carousel-item">
-                            <img src="/img/groupCar/prime/COLB.png" className="d-block mx-auto w-50" alt="Toyota Corolla" />
+                            <img src="/img/groupCar/prime/COLB.png" className="d-block mx-auto w-10" alt="Toyota Corolla" />
 
                             <div className="text-center p-3 mb-5 d-none d-md-block bg-dark bg-opacity-50 p-3 rounded mt-5 mt-5">
                                 <h4 className="text-white">Toyota Corolla</h4>
@@ -67,6 +81,17 @@ function Home() {
                             </div>
 
                         </div>
+
+
+                        <div className="carousel-item">
+                            <img src="/img/groupCar/prime/EQBL.png" className="d-block mx-auto w-10" alt="Chevrolet Equinox" />
+
+                            <div className="text-center p-3 mb-5 d-none d-md-block bg-dark bg-opacity-50 p-3 rounded mt-5 mt-5">
+                                <h4 className="text-white">Chevrolet Equinox</h4>
+                                <p className="text-white">Potência e conforto para viagens inesquecíveis.</p>
+                            </div>
+                        </div>
+
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -104,29 +129,29 @@ function Home() {
 
             <section className='section2'>
 
-                <div class="card-group">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                <div className="card-group">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
                 </div>
