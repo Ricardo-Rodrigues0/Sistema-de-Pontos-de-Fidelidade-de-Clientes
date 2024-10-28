f524dcde465e28d63d94daa86a94f680e181fae1
Ricardo <ricardo272530@gmail.com>
2024-10-28 12:08:53 -0300

    .

--- src/Home.js
+++ src/Home.js
@@ -136,67 +136,18 @@ function Home() {
 
 
             <section className='section2'>
-                <h1 className="mt-5 text-center">Explore Destinos Incríveis com seu Carro Alugado!</h1>
-                <p className="lead text-center mb-5">Escolha o destino que mais combina com a sua família e aproveite momentos inesquecíveis juntos.</p>
 
-
-                <div className="carousel-container">
-                    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-                        <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="..." />
                         </div>
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src="/img/viagens/image.png" className="img-viagem d-block mx-auto w-10" alt="Fiat Argo" />
-
-                                <div className="text-center d-none d-md-block rounded mt-5">
-                                    <h4 className="text-dark">Gramado, Rio Grande do Sul</h4>
-                                    <p className="text-dark">Gramado é conhecido por sua arquitetura europeia, gastronomia típica e clima serrano, sendo um dos destinos mais visitados no Brasil. Perfeito para passeios em família, a cidade oferece parques, museus, e eventos como o Natal Luz, uma das celebrações natalinas mais famosas do país. Explore as belas paisagens da Serra Gaúcha com o conforto do seu carro alugado e viva momentos mágicos.</p>
-                                </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
                             </div>
-
-                            <div className="carousel-item">
-                                <img src="/img/groupCar/pick-up/S10X.png" className="d-block mx-auto w-10" alt="Chevrolet S10" />
-
-                                <div className="text-center d-none d-md-block rounded mt-5">
-                                    <h4 className="text-dark">Categoria Picape - Chevrolet S10</h4>
-                                    <p className="text-dark">Considere também: Nissan Frontier, Mitsubishi L200, Montana, Fiat Toro e Toyota Hilux. <br></br> Descubra qual é o ideal para você!</p>
-                                </div>
-                            </div>
-
-
-                            <div className="carousel-item">
-                                <img src="/img/groupCar/prime/HAVA.png" className="d-block mx-auto w-10" alt="Haval H6" />
-
-                                <div className="text-center d-none d-md-block rounded mt-5">
-                                    <h4 className="text-dark">Categoria Prime - Haval H6</h4>
-                                    <p className="text-dark">Considere também: Audi A3, Jeep Commander, Toyota Corolla, Equinox e Song Plus. <br></br> Descubra qual é o ideal para você!</p>
-                                </div>
-
-                            </div>
-
-
-                            <div className="carousel-item">
-                                <img src="/img/groupCar/sedan/SETA.png" className="d-block mx-auto w-10" alt="Nissan Sentra" />
-
-                                <div className="text-center d-none d-md-block rounded mt-5">
-                                    <h4 className="text-dark">Categoria Sedan - Nissan Sentra</h4>
-                                    <p className="text-dark">Considere também: Fiat Cronos, Hyundai HB20, Onix, Virtus. <br></br> Descubra qual é o ideal para você!</p>
-                                </div>
-                            </div>
-
-
-                            <div className="carousel-item">
-                                <img src="/img/groupCar/suv/CRTA.png" className="d-block mx-auto w-10" alt="Hyundai Creta" />
-
-                                <div className="text-center d-none d-md-block rounded mt-5">
-                                    <h4 className="text-dark">Categoria Suv - Hyundai Creta</h4>
-                                    <p className="text-dark">Considere também: Fiat Fastback, Nissan Kicks, Nivus Highline, Fiat Pulse e Chevrolet Tracker. <br></br> Descubra qual é o ideal para você!</p>
-                                </div>
-                            </div>
-
                         </div>
                     </div>
                 </div>
