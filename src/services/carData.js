4dd70bf636da1e91a8348df78863c3e4cc9e2774
Ricardo <ricardo272530@gmail.com>
2024-11-09 12:22:00 -0300

    .

--- src/services/carData.js
+++ src/services/carData.js
@@ -1,23 +1,42 @@
 const cars = [
     {
-      name: 'Hyundai HB20',
-      img: '/image/groupCar/hatch/HB2X.png',
-      price: 200,
-      year: 2023,
-      features: 'Direção Elétrica, Ar-Condicionado, Bluetooth',
-      colors: ['black', 'silver', 'red'],
-      category: 'hatch',
+        name: 'Hyundai HB20',
+        img: './image/groupCar/hatch/HB2X.png',
+        price: 200,
+        year: 2023,
+        features: 'Direção Elétrica, Ar-Condicionado, Bluetooth',
+        description: 'Compacto e prático, o hatch é perfeito para o dia a dia, combinando agilidade e economia.',
+        description2: 'O Hyundai HB20 proporciona uma excelente experiência de viagem, ideal para quem deseja explorar novos destinos com segurança e conforto.',
+        colors: ['black', 'silver', 'red'],
+        category: 'hatch',
+        features2: [
+            { icon: '🚪', feature: '4 portas' },
+            { icon: '❄', feature: 'Ar-condicionado' },
+            { icon: '🔌', feature: 'Vidro elétrico' },
+            { icon: '🔒', feature: 'Trava elétrica' },
+            { icon: '🛡', feature: 'Air bag duplo' },
+            { icon: '🚦', feature: 'ABS' },
+        ]
     },
     {
-      name: 'Fiat Argo',
-      img: '/image/groupCar/hatch/Argo.png',
-      price: 250,
-      year: 2022,
-      features: 'Câmbio Manual, Ar-Condicionado, Vidro Elétrico',
-      colors: ['white', 'blue', 'gray'],
-      category: 'hatch',
+        name: 'Hyundai HB20',
+        img: './image/groupCar/hatch/HB2X.png',
+        price: 200,
+        year: 2023,
+        features: '',
+        description: 'Direção Elétrica, Ar-Condicionado, Bluetooth',
+        description2: 'Direção Elétrica, Ar-Condicionado, Bluetooth',
+        colors: ['black', 'silver', 'red'],
+        category: 'hatch',
+        features2: [
+            { icon: '🚪', feature: '4 portas' },
+            { icon: '❄', feature: 'Ar-condicionado' },
+            { icon: '🔌', feature: 'Vidro elétrico' },
+            { icon: '🔒', feature: 'Trava elétrica' },
+            { icon: '🛡', feature: 'Air bag duplo' },
+            { icon: '🚦', feature: 'ABS' },
+        ]
     },
-  ];
-  
-  export default cars;
-  
+];
+
+export default cars;
